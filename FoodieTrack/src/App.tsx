c6535bfdbb052bea1c80de262b2106631fb3ac4a
Jason <jason.chen20@gmail.com>
2026-02-06 22:50:26 -0500

    Basic React Components

---
+++ FoodieTrack/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>FoodieTrack</h1>
+    </>
+  )
+}
+
+export default App
