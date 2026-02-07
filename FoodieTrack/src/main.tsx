3484851e36ccd0f4ea2e4d4d6f6358f897a66658
Jason <jason.chen20@gmail.com>
2026-02-07 11:58:04 -0500

    Replaced domain and clientID in main

--- FoodieTrack/src/main.tsx
+++ FoodieTrack/src/main.tsx
@@ -4,14 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+// const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+// const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+// console.log('Auth0 Domain:', domain);
+// console.log('Auth0 Client ID:', clientId);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain="dev-wxq13kkpc6n1xlyo.us.auth0.com"
+      clientId="C4TBJ0kXjjPa52DU1YhdoQ1Q3lyJwuVi"
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
